6623e1998083b779dd311abb0c2780e502c79656
Abhishek Dave <abhidave2000@gmail.com>
2025-03-07 14:32:37 -0500

    Changes in Home page design and started with skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,15 @@
 function Skills(){
     return (
         <>
-            <h2>Skills</h2>
+            <div className="space-mono-regular">
+                <h2 className="page-heading skills-heading">Skills</h2>
+
+                <div className="d-flex row skills-page-div item-center gap-5">
+                    <div className="left-div"></div>
+                    <p>Programming Languages</p>
+                    <div className="right-div"></div>
+                </div>
+            </div>
         </>
     )
 }
