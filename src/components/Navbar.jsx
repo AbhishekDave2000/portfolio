9f7fa60584a8ca7eef5e2b88f677a39c8e021963
Abhishek Dave <abhidave2000@gmail.com>
2026-05-31 17:29:17 -0400

    feat(Setup) Add initial portfolio setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { NavLink, Link, useNavigate } from 'react-router-dom';
+
+const ACTIVE_STYLE = "text-xl font-medium border-b-2 border-amber-400" 
+const IN_ACTIVE_STYLE = "text-xl font-light text-amber-100" 
+
+const Navbar = () => {
+    return(
+        <div className='flex item-center justify-between gap-2 w-full sticky top-0 px-6 py-3 outline-none border-b-2 border-gray-900'>
+            <div>
+                <NavLink to="/" className='text-2xl font-medium text-amber-400 flex-1'>
+                    Abhishek Dave
+                </NavLink>
+            </div>
+            <div className='flex item-center justify-between gap-4'>
+                <NavLink to="/" className={({isActive}) => isActive ? ACTIVE_STYLE : IN_ACTIVE_STYLE}>
+                    Home
+                </NavLink>
+                <NavLink to="/about" className={({isActive}) => isActive ? ACTIVE_STYLE : IN_ACTIVE_STYLE}>
+                    About
+                </NavLink>
+                <NavLink to="/experience" className={({isActive}) => isActive ? ACTIVE_STYLE : IN_ACTIVE_STYLE}>
+                    Experience
+                </NavLink>
+                <NavLink to="/contact" className={({isActive}) => isActive ? ACTIVE_STYLE : IN_ACTIVE_STYLE}>
+                    Contact
+                </NavLink>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+const HomePage = () => {
+    return(
+        <>
+            <Navbar />
+            <div>This is a Home Page</div>
+        </>
+    )
+}
+
+export default HomePage;
