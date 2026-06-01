58faa44ac5257a34d95817451aa39c6570ec629e
Abhishek Dave <abhidave2000@gmail.com>
2026-05-31 22:57:20 -0400

    chore(Navbar) Changes in navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 import { NavLink, Link, useNavigate } from 'react-router-dom';
 
-const ACTIVE_STYLE = "text-xl font-medium border-b-2 border-amber-400" 
-const IN_ACTIVE_STYLE = "text-xl font-light text-amber-100" 
+const ACTIVE_STYLE = "font-medium border-b-2 border-amber-400" 
+const IN_ACTIVE_STYLE = "font-light text-amber-100" 
 
 const Navbar = () => {
     return(
-        <div className='flex item-center justify-between gap-2 w-full sticky top-0 px-6 py-3 outline-none border-b-2 border-gray-900'>
+        <div className='bg-gray-950 flex item-center justify-between gap-2 w-full sticky top-0 px-6 py-3 outline-none border-b-2 border-gray-900'>
             <div>
-                <NavLink to="/" className='text-2xl font-medium text-amber-400 flex-1'>
+                <NavLink to="/" className='text-2xl font-medium text-amber-300 flex-1'>
                     Abhishek Dave
                 </NavLink>
             </div>
-            <div className='flex item-center justify-between gap-4'>
+            <div className='flex item-center justify-between gap-4 flex-wrap text-lg'>
                 <NavLink to="/" className={({isActive}) => isActive ? ACTIVE_STYLE : IN_ACTIVE_STYLE}>
                     Home
                 </NavLink>
