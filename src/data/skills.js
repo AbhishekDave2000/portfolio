1d5d8c164c352f503a50239d4829c97f0601b2af
Abhishek Dave <abhidave2000@gmail.com>
2026-06-02 22:50:14 -0400

    feat(data) Add data for the skills section

---
+++ src/data/skills.js
@@ -0,0 +1,47 @@
+export const skills = {
+    languages: [
+        "Ruby",
+        "Javascript",
+        "Typescript",
+        "SQL",
+        "Python",
+        "C#",
+        "Java",
+    ],
+
+    frameworks: [
+        "Ruby on Rails",
+        "React.Js",
+        "Node.Js",
+        "Express.Js",
+        "ASP.NET",
+        "JQUery",
+        "Tailwind CSS",
+    ],
+
+    databases: [
+        "MySQL",
+        "PostgreSQL",
+        "MongoDB",
+    ],
+
+    tools: [
+        "Redis",
+        "Git",
+        "Gitlab",
+        "Docker",
+        "AWS (S3)",
+        "CI/CD pipelines",
+        "RESTFul APIs",
+        "Websocket",
+        "Rspec",
+        "Postman",
+    ],
+
+    methods: [
+        "Agile/Scrum",
+        "Test Driven Development (TDD)",
+        "Object Oriented Programming",
+        "Microservice Architecture",
+    ]
+}
