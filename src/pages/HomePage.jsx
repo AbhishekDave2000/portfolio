2c9f9a3c5ec992b7f7278b5ceea618231c39f5d8
Abhishek Dave <abhidave2000@gmail.com>
2026-06-04 22:13:15 -0400

    feat(Home) Add Education card data and also change the css styles in the main page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,9 @@ import SkillsRow from "../components/SkillsRow";
 import ExperienceCard from "../components/ExperienceCard";
 import { skills } from "../data/skills";
 import { experiences } from "../data/experience";
+import { education } from "../data/education";
 import { BsJavascript } from "react-icons/bs";
+import EducationCard from "../components/EducationCard";
 
 const HomePage = () => {
     return(
@@ -14,7 +16,7 @@ const HomePage = () => {
             <Navbar />
 
             {/* Hero Section */}
-            <section id="hero" className="flex flex-col items-start justify-center w-full max-w-5xl h-auto mx-auto tracking-wide px-5 mt-20 mb-16">
+            <section id="hero" className="flex flex-col items-start justify-center w-full max-w-5xl h-auto mx-auto tracking-wide px-5 my-5 pt-20 pb-10">
                 
                 <span className="max-w-fit font-light text-3xl text-amber-300 mb-1 animate-bounce">Full Stack Software Developer</span >
                 
@@ -35,7 +37,7 @@ const HomePage = () => {
             <hr className="text-gray-900" />
             
             {/* About Section */}
-            <section id="about" className="flex flex-col item-center justify-center w-full max-w-5xl mx-auto py-16">
+            <section id="about" className="flex flex-col item-center justify-center w-full max-w-5xl mx-auto my-5 pt-20 pb-10">
                 <SectionHeading number="01" heading="ABOUT ME" />
                 <div className="flex flex-row gap-4">
                     {/* Left About Section */}
@@ -61,7 +63,7 @@ const HomePage = () => {
             <hr className="text-gray-900" />
 
             {/* Skills Section */}
-            <section id="skills" className="flex flex-col item-center justify-center mt-5 w-full max-w-5xl mx-auto py-16">
+            <section id="skills" className="flex flex-col item-center justify-center my-5 pt-20 pb-10 w-full max-w-5xl mx-auto">
                 <SectionHeading number="02" heading="SKILLS" />
                 <div className="flex flex-col gap-4">
                     <SkillsRow heading="LANGUAGES" skillType="primary" skills={skills.languages} />
@@ -72,20 +74,26 @@ const HomePage = () => {
                 </div>
             </section>
 
-            {/* Experience Section */}
-            <section id="experience" className="flex flex-col item-center justify-center mt-5 w-full max-w-5xl mx-auto">
-                <SectionHeading number="03" heading="EXPERIENCE" />
+            <hr className="text-gray-900" />
 
+            {/* Experience Section */}
+            <section id="experience" className="flex flex-col item-center justify-center my-5 pt-20 pb-10 w-full max-w-5xl mx-auto">
+                <SectionHeading number="03" heading="EXPERIENCE" />
                 {experiences.map((exp) => <ExperienceCard key={exp.id} exp={exp} />)}
             </section>
 
+            <hr className="text-gray-900" />
+
             {/* Education Section */}
-            <section id="education" className="flex flex-col item-center justify-center mt-5 w-full max-w-5xl mx-auto">
+            <section id="education" className="flex flex-col item-center justify-center my-5 pt-20 pb-10 w-full max-w-5xl mx-auto">
                 <SectionHeading number="04" heading="EDUCATION" />
+                <div className="flex flex-row flex-wrap justify-between gap-6 mt-2">
+                    {education.map( (edu, index) => <EducationCard key={index} edu={edu} />)}
+                </div>
             </section>
 
             {/* Contact */}
-            <section id="contact" className="flex flex-col item-center justify-center mt-5 w-full max-w-5xl mx-auto mb-5">
+            <section id="contact" className="flex flex-col item-center justify-center my-5 pt-20 pb-10 w-full max-w-5xl mx-auto mb-5">
                 <SectionHeading number="05" heading="CONTACT" />
             </section>
         </>
