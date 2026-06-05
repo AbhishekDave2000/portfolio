50ef8aaed2fd949bb9cfb3388255f0a346761de9
Abhishek Dave <abhidave2000@gmail.com>
2026-06-04 22:12:41 -0400

    feat(EducationCard) Add education card component

---
+++ src/components/EducationCard.jsx
@@ -0,0 +1,17 @@
+const EducationCard = ({edu}) => {
+    return(
+        <>
+            <div className="flex flex-row min-w-max">
+                <div className="h-full w-1 bg-amber-400 rounded-4xl"></div>
+                <div className="flex flex-col gap-1 bg-zinc-900 p-6 rounded-xl">
+                    <h2 className="text-4xl text-amber-300">{edu.degree}</h2>
+                    <p className="text-2xl">{edu.major}</p>
+                    <span className="text-zinc-400 text-xl">{edu.institute}, {edu.location}</span>
+                    <span className="text-zinc-400 text-xl">{edu.duration} . GPA {edu.gpa} </span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default EducationCard;
