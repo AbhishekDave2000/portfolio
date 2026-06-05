dce87bc5e35330becc33919bdb65dcfcbfcf9a10
Abhishek Dave <abhidave2000@gmail.com>
2026-06-04 22:12:06 -0400

    feat(Education) Add education data file

---
+++ src/data/education.js
@@ -0,0 +1,18 @@
+export const education = [
+    {
+        degree:     "Post Graduate Certificate",
+        major:      "Full Stack Software Development",
+        institute:  "Lambton College",
+        location:   "Toronto, ON, Canada",
+        duration:    "Jan 2024 - Aug 2025",
+        gpa:        "3.52/4.0",
+    },
+    {
+        degree:     "Bachelor Of Technology",
+        major:      "Information Technology",
+        institute:  "Gujarat Technological University",
+        location:   "Gujarat, India",
+        duration:    "Jul 2018 - Jun 2022",
+        gpa:        "9.51/10.0",
+    },
+] 
