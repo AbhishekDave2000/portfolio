747c1c2c300da67f563d48b0ef763c94fe4b6f84
Abhishek Dave <abhidave2000@gmail.com>
2026-06-09 21:48:34 -0400

    chore(Design) Change in home page section spaces

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,20 +27,20 @@ const HomePage = () => {
             <ScrollUpButton />
 
             {/* Hero Section */}
-            <section id="hero" className="flex flex-col items-start justify-center w-full max-w-5xl h-auto mx-auto tracking-wide px-5 my-5 pt-20 pb-10">
+            <section id="hero" className="flex flex-col items-start justify-center w-full max-w-5xl h-auto mx-auto tracking-wide px-5 py-20">
                 <HeroSection />
             </section>
             <div className={`h-px w-full bg-linear-to-r from-transparent via-zinc-700 to-transparent`} />
             
             {/* About Section */}
-            <section id="about" className="flex flex-col item-center justify-center w-full max-w-5xl mx-auto my-5 pt-20 pb-10">
+            <section id="about" className="flex flex-col item-center justify-center w-full max-w-5xl mx-auto py-20">
                 <SectionHeading number="01" heading="ABOUT ME" />
                 <AboutSection />
             </section>
             <div className={`h-px w-full bg-linear-to-r from-transparent via-zinc-700 to-transparent`} />
 
             {/* Skills Section */}
-            <section id="skills" className="flex flex-col item-center justify-center my-5 pt-20 pb-10 w-full max-w-5xl mx-auto">
+            <section id="skills" className="flex flex-col item-center justify-center py-20 w-full max-w-5xl mx-auto">
                 <SectionHeading number="02" heading="SKILLS" />
                 <div className="flex flex-col gap-4">
                     <SkillsRow heading="LANGUAGES" skillType="primary" skills={skills.languages} />
@@ -53,14 +53,14 @@ const HomePage = () => {
             <div className={`h-px w-full bg-linear-to-r from-transparent via-zinc-700 to-transparent`} />
 
             {/* Experience Section */}
-            <section id="experience" className="flex flex-col item-center justify-center my-5 pt-20 pb-10 w-full max-w-5xl mx-auto">
+            <section id="experience" className="flex flex-col item-center justify-center py-20 w-full max-w-5xl mx-auto">
                 <SectionHeading number="03" heading="EXPERIENCE" />
                 {experiences.map((exp, index) => <ExperienceCard key={exp.id} exp={exp} isLast={index === experiences.length - 1} />)}
             </section>
             <div className={`h-px w-full bg-linear-to-r from-transparent via-zinc-700 to-transparent`} />
 
             {/* Project Section */}
-            <section id="projects" className="flex flex-col item-center justify-center my-5 pt-20 pb-10 w-full max-w-5xl mx-auto">
+            <section id="projects" className="flex flex-col item-center justify-center py-20 w-full max-w-5xl mx-auto">
                 <SectionHeading number="04" heading="PROJECTS" />
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-stretch">
                     {projects.map((project, index) => <ProjectCard key={index} project={project} index={index} />)}
@@ -69,7 +69,7 @@ const HomePage = () => {
             <div className={`h-px w-full bg-linear-to-r from-transparent via-zinc-700 to-transparent`} />
 
             {/* Education Section */}
-            <section id="education" className="flex flex-col item-center justify-center my-5 pt-20 pb-10 w-full max-w-5xl mx-auto">
+            <section id="education" className="flex flex-col item-center justify-center py-20 w-full max-w-5xl mx-auto">
                 <SectionHeading number="05" heading="EDUCATION" />
                 <div className="flex flex-row flex-wrap justify-between gap-6 mt-2">
                     {education.map( (edu, index) => <EducationCard key={index} edu={edu} />)}
@@ -78,7 +78,7 @@ const HomePage = () => {
             <div className={`h-px w-full bg-linear-to-r from-transparent via-zinc-700 to-transparent`} />
 
             {/* Contact */}
-            <section id="contact" className="flex flex-col item-center justify-center my-5 pt-20 pb-10 w-full max-w-5xl mx-auto mb-5">
+            <section id="contact" className="flex flex-col item-center justify-center py-20 w-full max-w-5xl mx-auto mb-5">
                 <SectionHeading number="06" heading="CONTACT" />
                 <div className="max-w-7xl mx-auto mt-5">
                     <div className="grid grid-cols-2 gap-8">
